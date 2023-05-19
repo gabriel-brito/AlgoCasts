c5c0b2226ef7db35ceef0232a38a84651bc6b56c
Gabriel Santos <mfkrgabriel@gmail.com>
2023-05-19 10:29:15 +0300

    fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,21 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+// BIG O: O(n)
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 == 0 && i % 5 === 0) {
+      console.log('fizzbuzz')
+    } else if (i % 3 == 0) {
+      console.log('fizz')
+    } else if (i % 5 === 0) {
+      console.log('buzz')
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+// Lecture solution was the same as mine.
 
 module.exports = fizzBuzz;
