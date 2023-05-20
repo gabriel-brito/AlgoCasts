e43a75fb85c42f431b592844824d3c3f188bc9a6
Gabriel Santos <mfkrgabriel@gmail.com>
2023-05-20 16:12:34 +0300

    find vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,20 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const letters = ['a', 'e', 'i', 'o', 'u'];
+  const cleanString = str.replace(/\^W/g, '').toLowerCase().split('').sort().join('')
+  let count = 0;
+  
+  for (let char in cleanString) {
+      const element = cleanString.charAt(char);
+      
+      if(letters.some(letter => letter === element)) {
+          count++
+      }
+  }
+  
+  return count;
+}
 
 module.exports = vowels;
