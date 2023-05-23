9b4b5e65e64f3e7f0d4ef8deb8e495f636637214
Gabriel Santos <mfkrgabriel@gmail.com>
2023-05-23 17:00:52 +0300

    Selection sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -14,7 +14,22 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
 
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+
+    if (indexOfMin !== i) {
+      [arr[indexOfMin], arr[i]] = [arr[i], arr[indexOfMin]]
+    }
+  }
+
+
+  return arr;
 }
 
 function mergeSort(arr) {
