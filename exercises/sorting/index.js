de5adb6e51beff3d52a247423bbe46bf56cdfcf6
Gabriel Santos <mfkrgabriel@gmail.com>
2023-05-23 14:11:15 +0300

    bubblesort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,7 +2,15 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[i] < arr[j]) {
+            [arr[i], arr[j]] = [arr[j], arr[i]]
+        }
+    }
+  }
 
+  return arr;
 }
 
 function selectionSort(arr) {
