0a883fe3d395a821a8a04c68114898e2c84f9bd8
Gabriel Santos <mfkrgabriel@gmail.com>
2023-05-23 12:35:30 +0300

    level width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,24 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const s = 'stop';
+  const tree = [root, s];
+  const counters = [0];
+
+  while(tree.length > 1) {
+    const node = tree.shift();
+
+    if (node === s) {
+      counters.push(0)
+      tree.push(s);
+    } else {
+      tree.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+
+  return counters;
+}
 
 module.exports = levelWidth;
