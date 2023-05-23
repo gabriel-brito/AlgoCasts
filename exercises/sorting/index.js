fadda38408b54209d869be704013aed4d9fc5d25
Gabriel Santos <mfkrgabriel@gmail.com>
2023-05-23 17:26:07 +0300

    Merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -33,11 +33,33 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
 
+  const middle = Math.floor(arr.length / 2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+  const results = [];
 
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  if (left.length) results.push(...left);
+
+  if (right.length) results.push(...right);
+
+  return results
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
